d34816e5be083e1ceaca734614cbe0556739e692
Rohan Surana <rohansurana2810@gmail.com>
2021-04-17 17:31:39 +0530

    tf2 model

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
 import argparse
+import os
 
-import tensorflow as tf
-from tensorflow.python.lib.io import file_io
-
-from model import cyclegan
-from style_classifier import Classifer
-
-tf.set_random_seed(19)
+from model import CycleGAN
+from style_classifier import Classifier
 
 parser = argparse.ArgumentParser(description='')
-parser.add_argument('--dataset_dir', dest='dataset_dir', default='JAZZ2ROCK', help='path of the dataset')
-parser.add_argument('--dataset_A_dir', dest='dataset_A_dir', default='JC_J', help='path of the dataset of domain A')
-parser.add_argument('--dataset_B_dir', dest='dataset_B_dir', default='JC_C', help='path of the dataset of domain B')
-parser.add_argument('--epoch', dest='epoch', type=int, default=100, help='# of epoch')
+parser.add_argument('--dataset_A_dir', dest='dataset_A_dir', default='CP_C', help='path of the dataset of domain A')
+parser.add_argument('--dataset_B_dir', dest='dataset_B_dir', default='CP_P', help='path of the dataset of domain B')
+parser.add_argument('--epoch', dest='epoch', type=int, default=10, help='# of epoch')
 parser.add_argument('--epoch_step', dest='epoch_step', type=int, default=10, help='# of epoch to decay lr')
-parser.add_argument('--batch_size', dest='batch_size', type=int, default=1, help='# images in batch')  # 16 -> 1
-parser.add_argument('--train_size', dest='train_size', type=int, default=1e8, help='# images used to train')
-parser.add_argument('--load_size', dest='load_size', type=int, default=286, help='scale images to this size')
-parser.add_argument('--fine_size', dest='fine_size', type=int, default=128, help='then crop to this size')
+parser.add_argument('--b-atch_size', dest='batch_size', type=int, default=4, help='# images in batch')
 parser.add_argument('--time_step', dest='time_step', type=int, default=64, help='time step of pianoroll')
 parser.add_argument('--pitch_range', dest='pitch_range', type=int, default=84, help='pitch range of pianoroll')
 parser.add_argument('--ngf', dest='ngf', type=int, default=64, help='# of gen filters in first conv layer')
@@ -40,40 +32,30 @@ parser.add_argument('--test_dir', dest='test_dir', default='./test', help='test 
 parser.add_argument('--log_dir', dest='log_dir', default='./log', help='logs are saved here')
 parser.add_argument('--L1_lambda', dest='L1_lambda', type=float, default=10.0, help='weight on L1 term in objective')
 parser.add_argument('--gamma', dest='gamma', type=float, default=1.0, help='weight of extra discriminators')
-parser.add_argument('--use_midi_G', dest='use_midi_G', type=bool, default=False, help='select generator for midinet')
-parser.add_argument('--use_midi_D', dest='use_midi_D', type=bool, default=False, help='select disciminator for midinet')
-parser.add_argument('--use_lsgan', dest='use_lsgan', type=bool, default=False, help='gan loss defined in lsgan')
 parser.add_argument('--max_size', dest='max_size', type=int, default=50,
                     help='max size of image pool, 0 means do not use image pool')
-parser.add_argument('--sigma_c', dest='sigma_c', type=float, default=1.0, help='sigma of gaussian noise of classifiers')
-parser.add_argument('--sigma_d', dest='sigma_d', type=float, default=1.0,
+parser.add_argument('--sigma_c', dest='sigma_c', type=float, default=0.01,
+                    help='sigma of gaussian noise of classifiers')
+parser.add_argument('--sigma_d', dest='sigma_d', type=float, default=0.01,
                     help='sigma of gaussian noise of discriminators')
-parser.add_argument('--model', dest='model', default='base', help='three different models, base, partial, full')
-parser.add_argument('--type', dest='type', default='cyclegan', help='cyclegan or classifier')
+parser.add_argument('--model', dest='model', default='full', help='three different models, base, partial, full')
+parser.add_argument('--type', dest='type', default='classifier', help='cyclegan or classifier')
 
 args = parser.parse_args()
 
-
-def main(_):
-    if not file_io.file_exists(args.checkpoint_dir):
-        file_io.create_dir(args.checkpoint_dir)
-    if not file_io.file_exists(args.sample_dir):
-        file_io.create_dir(args.sample_dir)
-    if not file_io.file_exists(args.test_dir):
-        file_io.create_dir(args.test_dir)
-
-    tfconfig = tf.ConfigProto(allow_soft_placement=True)
-    tfconfig.gpu_options.allow_growth = True
-    with tf.Session(config=tfconfig) as sess:
-
-        if args.type == 'cyclegan':
-            model = cyclegan(sess, args)
-            model.train(args) if args.phase == 'train' else model.test(args)
-
-        if args.type == 'classifier':
-            classifier = Classifer(sess, args)
-            classifier.train(args) if args.phase == 'train' else classifier.test(args)
-
-
 if __name__ == '__main__':
-    tf.app.run()
+
+    if not os.path.exists(args.checkpoint_dir):
+        os.makedirs(args.checkpoint_dir)
+    if not os.path.exists(args.sample_dir):
+        os.makedirs(args.sample_dir)
+    if not os.path.exists(args.test_dir):
+        os.makedirs(args.test_dir)
+
+    if args.type == 'cyclegan':
+        model = CycleGAN(args)
+        model.train(args) if args.phase == 'train' else model.test(args)
+
+    if args.type == 'classifier':
+        classifier = Classifier(args)
+        classifier.train(args) if args.phase == 'train' else classifier.test(args)
